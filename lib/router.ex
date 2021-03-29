4e5fb4744be1751595686c38e48ad576fbc6425a
Jeremiah Chen <56935165+jeremiahcchen@users.noreply.github.com>
2021-03-28 17:19:43 -0700

    Worked on acquiring data from PostgreSQL

--- lib/router.ex
+++ lib/router.ex
@@ -10,6 +10,12 @@ defmodule SiteEx.Router do
     at: "/",
     from: :site_ex
 
+  #This is the relevant piece of code which makes it so that all requests for resources with the
+  #path /lib/web are served from the directory in the backend: lib/web
+  plug Plug.Static,
+    at: "/lib/web",
+    from: "lib/web"
+
   plug :match
   plug Plug.Parsers,
     parsers: [:json],
@@ -39,6 +45,15 @@ defmodule SiteEx.Router do
     |> send_resp(200, "{\"result\": \"success\"}")
   end
 
+  get "lobby_list" do
+
+    map = Lobbies.Repo.all Lobbies.Room
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(map))
+  end
+
   get "/lobbies" do
     conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
@@ -50,6 +65,16 @@ defmodule SiteEx.Router do
     send_resp(conn, 200, application_html())
   end
   #---------------------------
+
+  # get "/hello/*glob" do
+  #   send_resp(conn, 200, "route after /hello: #{inspect glob}")
+  # end
+
+  # get "/lib/web/p_lobbylist.js" do
+  #   conn
+  #   |> put_resp_header("content-type", "text/javascript")
+  #   |> send_file(200, "lib/web/p_lobbylist.js")
+  # end
 
   # get "/lib/web/script.js" do
   #   conn
